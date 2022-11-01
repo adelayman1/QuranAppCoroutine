f707fa4112b098b45a042b01c505c7a1cb8ebbf5
adelayman1 <85571327+adelayman1@users.noreply.github.com>
2022-11-02 01:22:27 +0200

    Update SurahApiModule.kt

--- app/src/main/java/com/adel/myquran/data/di/module/SurahApiModule.kt
+++ app/src/main/java/com/adel/myquran/data/di/module/SurahApiModule.kt
@@ -1,12 +1,13 @@
 package com.adel.myquran.data.di.module
 
-import com.adel.myquran.data.api.SurahApiService
+import com.example.myqurancore.data.sources.remote.endPoints.SurahApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import javax.inject.Singleton
+
 @Module(includes = [NetworkModule::class])
 @InstallIn(SingletonComponent::class)
 class SurahApiModule {
